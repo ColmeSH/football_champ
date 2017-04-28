7653084198638d0f83c46cfa53c0fbb4464cdc3e
ColmeSH <mauriziocontatto@gmail.com>
2017-04-28 17:45:04 +0200

    update team championship

--- Classes/team.py
+++ Classes/team.py
@@ -2,6 +2,8 @@ class Team():
 
     def __init__(self, name):
         self.name = name
+        self.points = 0
+        self.teamid = 0
 
     def __str__(self):
         return "TEAM: {}".format(self.name)
--- main.py
+++ main.py
@@ -1,40 +1,52 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from Classes.team import Team
+
 app = Flask(__name__)
 teams = []
 print teams
 
 
+def generate_id(teams):
+    try:
+        return teams[-1].teamid + 1
+    except:
+        return 1
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', teams=teams)
 
 
 @app.route('/create', methods=['GET', 'POST'])
 def create():
-
     if request.method == 'POST':
         print 'sono in post'
-        #create team with name passed by post method
-        t = Team(request.form['team'])
-        teams.append(t)
+        # create team with name passed by post method
+        if request.form['name'] != "":
+            t = Team(request.form['name'])
+            t.teamid = generate_id(teams)
+            print t.teamid
 
-        return request.form['team']
+            teams.append(t)
+            print teams
+            return redirect('/')
+        else:
+            msg = "Please insert a football team name!"
+            return render_template('createteam.html', msg=msg)
+
     else:
         print 'sono in get'
         return render_template('createteam.html')
 
+@app.route('/details/team/<teamid>', methods = ['GET'])
+def details(teamid):
+    return render_template('teamdetails.html')
 
 
 
 
 
 
-
-
-
-
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
